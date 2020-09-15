720f83a1ca016ffeb43c9badea70d10b23c9373c
Camilo318 <camiloa0318@gmail.com>
2020-09-15 13:53:51 -0500

    Rename svg files

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Option from './Option'
+import adventure from '../assets/images/undraw_adventure.svg'
 
 const Card = () => {
     return (
         <div
-        className='bg-white rounded-24 px-4 sm:px-8 py-8 sm:py-16 relative w-full sm:max-w-lg mx-auto'>
+        className='bg-white rounded-24 px-2 sm:px-8 py-16 relative w-full sm:max-w-lg mx-auto'>
             <h3 className='text-2xl font-bold text-heading'>What is tha capital Colombia?</h3>
             <Option />
             <Option />
             <Option />
             <Option />
-            <img src="" alt=""/>
+            <img src={adventure}
+            className='absolute right-0 top-0 -mt-20'
+            alt="Test"/>
         </div>
     )
 }
