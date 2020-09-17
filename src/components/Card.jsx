8aef38b1930bddd88b4ddd54389cfbe8f814a7a9
Camilo318 <camiloa0318@gmail.com>
2020-09-17 18:53:14 -0500

    Add flag image Delete svg image Fix font-size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,23 +13,20 @@ const Card = () => {
     useEffect(() => {
         fetch('https://restcountries.eu/rest/v2/name/colombia')
         .then(res => res.json())
-        .then(data => {
-            setData(data)
-        })
+        .then(data => setData(data))
         .catch(console.log)
 
     }, [])
 
     return (
         <div className='bg-white rounded-24 px-4 sm:px-8 py-8 sm:py-16 relative max-w-lg'>
-            <h3 className='text-2xl font-bold text-heading'>What is the capital Colombia?</h3>
+            <img src={data[0].flag} alt="Country Flag"
+            className="w-32 mx-auto"/>
+            <h3 className='text-lg sm:text-2xl font-bold text-heading mt-6'>What is the capital Colombia?</h3>
             <Option opt='A' value={data[0].capital}/>
             <Option opt='B' value={'Billie'}/>
             <Option opt='C' value={'Billie'}/>
             <Option opt='D' value={'Billie'}/>
-            <img src={adventure}
-            className='card-image absolute right-0 w-1/3 max-w-full'
-            alt="Test"/>
         </div>
     )
 }
--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Option = ({opt, value}) => {
     return (
         <div
-        className='option'>
+        className='option text-base sm:text-lg'>
 
         <span className='text-2xl mr-12'>{opt}</span>    
         <span>{value}</span>
